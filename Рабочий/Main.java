f55b05d9ec6b48c2a4f29ba745a4bbb550b53503
FominykhVE <113411788+FominykhVE@users.noreply.github.com>
2022-10-16 21:18:07 +0300

    Update Main.java

--- Рабочий/Main.java
+++ Рабочий/Main.java
@@ -7,32 +7,3 @@ public class Main {
 
         worker.start();
     }
-
-
-    static class Worker{
-
-        private OnTaskDoneListener callBack;
-
-
-
-        public Worker(OnTaskDoneListener callBack){
-            this.callBack = callBack;
-        }
-
-        public void start() {
-            for (int i = 0; i < 100; i++) {
-                callBack.onDone("Task " + i + " is done");
-            }
-        }
-
-    }
-
-    @FunctionalInterface
-    public interface OnTaskDoneListener {
-
-
-        void onDone(String value);
-    }
-
-
-}
